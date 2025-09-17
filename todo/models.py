ca4a1412a4036918e380b243f84e5cacff2a28e6
Артём Мамаев <fanfir.artem@gmail.com>
2025-09-17 16:18:06 +0500

    models.py

--- todo/models.py
+++ todo/models.py
@@ -1,12 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
+from django.contrib.auth.models import User  # Добавьте этот импорт, если используете стандартную модель User
 
 class Task(models.Model):
-    user = model.ForeignKey( User, on_delete=model.CASCADE)
-    title= model.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Замените model на models
+    title = models.CharField(max_length=200)
     created_date = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.title
